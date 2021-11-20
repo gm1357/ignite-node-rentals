ffe8fb233369516716433fa99db0e6c68e711120
Gabriel <g_machado1@hotmail.com>
2021-11-20 15:43:52 -0300

    Created new route to list rentals by user

--- src/modules/rentals/repositories/IRentalsRepository.ts
+++ src/modules/rentals/repositories/IRentalsRepository.ts
@@ -2,6 +2,7 @@ import { ICreateRentalsDTO } from "../dtos/ICreateRentalsDTO";
 import { Rental } from "../infra/typeorm/entities/rental";
 
 interface IRentalsRepository {
+    findByUser(user_id: string): Promise<Rental[]>;
     findOpenRentalByUser(user_id: string): Promise<Rental | undefined>;
     findOpenRentalByCar(car_id: string): Promise<Rental | undefined>;
     create(data: ICreateRentalsDTO): Promise<Rental>;
