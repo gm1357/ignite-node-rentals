fd86afbc6549536965d5bcb83f61e3a34660dbb4
Gabriel <g_machado1@hotmail.com>
2021-11-28 15:16:52 -0300

    Fixed tests

---
+++ src/modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory.ts
@@ -0,0 +1,52 @@
+import { ICreateUserTokenDTO } from "@modules/accounts/dtos/ICreateUserTokenDTO";
+import { UserTokens } from "@modules/accounts/infra/typeorm/entities/userTokens";
+
+import { IUsersTokensRepository } from "../IUsersTokensRepository";
+
+class UsersTokensRepositoryInMemory implements IUsersTokensRepository {
+    usersTokens: UserTokens[] = [];
+
+    async create({
+        expires_date,
+        refresh_token,
+        user_id,
+    }: ICreateUserTokenDTO): Promise<UserTokens> {
+        const userToken = new UserTokens();
+
+        Object.assign(userToken, {
+            expires_date,
+            refresh_token,
+            user_id,
+        });
+
+        this.usersTokens.push(userToken);
+
+        return userToken;
+    }
+
+    async findByUserIdAndRefreshToken(
+        user_id: string,
+        refresh_token: string
+    ): Promise<UserTokens | undefined> {
+        return this.usersTokens.find(
+            (ut) => ut.user_id === user_id && ut.refresh_token && refresh_token
+        );
+    }
+
+    async deleteById(id: string): Promise<void> {
+        const userToken = this.usersTokens.find((ut) => ut.id === id);
+        if (userToken) {
+            this.usersTokens.splice(this.usersTokens.indexOf(userToken));
+        }
+    }
+
+    async findByRefreshToken(
+        refresh_token: string
+    ): Promise<UserTokens | undefined> {
+        return this.usersTokens.find(
+            (ut) => ut.refresh_token === refresh_token
+        );
+    }
+}
+
+export { UsersTokensRepositoryInMemory };
--- src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
+++ src/modules/accounts/useCases/authenticateUser/AuthenticateUserUseCase.spec.ts
@@ -1,19 +1,29 @@
 import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
 import { UserRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UserRepositoryInMemory";
+import { UsersTokensRepositoryInMemory } from "@modules/accounts/repositories/in-memory/UsersTokensRepositoryInMemory";
+import { DayjsDateProvider } from "@shared/container/providers/DateProvider/implementations/DayjsDateProvider";
 import { AppError } from "@shared/errors/AppError";
 
 import { CreateUserUseCase } from "../createUser/CreateUserUseCase";
 import { AuthenticateUserUseCase } from "./AuthenticateUserUseCase";
 
 let userRepositoryInMemory: UserRepositoryInMemory;
+let usersTokensRepositoryInMemory: UsersTokensRepositoryInMemory;
+
+let dayjsDateProvider: DayjsDateProvider;
+
 let authenticateUserUseCase: AuthenticateUserUseCase;
 let createUserUseCase: CreateUserUseCase;
 
 describe("Authenticate User", () => {
     beforeEach(() => {
         userRepositoryInMemory = new UserRepositoryInMemory();
+        usersTokensRepositoryInMemory = new UsersTokensRepositoryInMemory();
+        dayjsDateProvider = new DayjsDateProvider();
         authenticateUserUseCase = new AuthenticateUserUseCase(
-            userRepositoryInMemory
+            userRepositoryInMemory,
+            usersTokensRepositoryInMemory,
+            dayjsDateProvider
         );
         createUserUseCase = new CreateUserUseCase(userRepositoryInMemory);
     });
